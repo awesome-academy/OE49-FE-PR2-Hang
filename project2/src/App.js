52b737eb31c71f773bfadeacf67bd302b9d31f02
vuhang1712 <vuhang1939@gmail.com>
2022-04-05 09:43:08 +0700

    Tạo Footer

--- project2/src/App.js
+++ project2/src/App.js
@@ -1,10 +1,12 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Header from "./components/Header/Header";
+import Footer from "./components/Footer/Footer";
 
 function App() {
   return (
     <>
       <Header />
+      <Footer />
     </>
   );
 }
